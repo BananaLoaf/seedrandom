78120747975a75fd3dfba43d4de01650e65fe191
BananaLoaf <bananaloaf@protonmail.com>
2020-08-17 13:55:05 +0300

    Minor changes

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
+# Local
 from setuptools import setup, find_packages
+
+# Project
 from seedrandom import PACKAGE_NAME, __version__
+
 
 with open("README.md", "r") as file:
     long_description = file.read()
@@ -12,6 +16,8 @@ setup(
     # Metadata for PyPi
     author="BananaLoaf",
     author_email="bananaloaf@protonmail.com",
+    # maintainer="BananaLoaf",
+    # maintainer_email="bananaloaf@protonmail.com",
     license="MIT",
 
     description="Deterministic seeded RNG",
@@ -20,10 +26,15 @@ setup(
     keywords=["random", "seed", "generator", "hash", "int", "float", "bool", "bytes", "rng", "deterministic"],
 
     url=f"https://github.com/BananaLoaf/seedrandom",
+    # download_url=None,
+    # project_urls={
+    #     "Lord and Saviour": "https://stackoverflow.com/"
+    # },
+
     classifiers=[
         "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3"])
+        "Programming Language :: Python :: 3"
+    ])
